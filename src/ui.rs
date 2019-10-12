25e0ac8060ae96e51bbd437e35afb5cb5f362a7c
Frank Groeneveld <frank@frankgroeneveld.nl>
2019-10-12 15:16:29 +0200

    Add popup dialog

--- src/ui.rs
+++ src/ui.rs
@@ -38,13 +38,38 @@ pub fn main_window() {
 
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
+    let container = gtk::HeaderBar::new();
+
+    container.set_title(Some("Harvest"));
+    container.set_show_close_button(true);
 
     window.set_title("Harvest");
+    window.set_titlebar(Some(&container));
     window.set_border_width(10);
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(350, 70);
 
     let button = gtk::Button::new_with_label("Start");
+    let application_clone = application.clone();
+    let window_clone = window.clone();
+    button.connect_clicked(move |_| {
+        //print_time_entries();
+
+        let popup = gtk::Window::new(gtk::WindowType::Toplevel);
+
+        application_clone.add_window(&popup);
+
+        popup.set_title("Add time entry");
+        popup.set_default_size(400, 200);
+        popup.set_modal(true);
+        popup.set_transient_for(Some(&window_clone));
+        popup.set_type_hint(gdk::WindowTypeHint::Dialog);
+
+        popup.connect_delete_event(|_, _| Inhibit(false));
+
+        popup.show_all();
+    });
+
     let list_store = gtk::ListStore::new(&[gtk::Type::String]);
 
     list_store.set(&list_store.append(), &[0], &[&"Test".to_string()]);
@@ -52,11 +77,8 @@ fn build_ui(application: &gtk::Application) {
     let cell = gtk::CellRendererText::new();
     combo_box.pack_start(&cell, true);
     combo_box.add_attribute(&cell, "text", 0);
-    button.connect_clicked(|_| {
-        print_time_entries();
-    });
 
-    // window.add(&button);
+    container.pack_start(&button);
     window.add(&combo_box);
 
     window.show_all();
