701ed6f24411a7845596ec8e0161874540a26dce
Frank Groeneveld <frank@frankgroeneveld.nl>
2020-06-11 19:54:19 +0200

    Use clone! macro

--- src/ui.rs
+++ src/ui.rs
@@ -3,6 +3,24 @@ use gio::prelude::*;
 use gtk::prelude::*;
 use std::sync::mpsc;
 
+/* handy gtk callback clone macro taken from https://gtk-rs.org/docs-src/tutorial/closures */
+macro_rules! clone {
+    (@param _) => ( _ );
+    (@param $x:ident) => ( $x );
+    ($($n:ident),+ => move || $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move || $body
+        }
+    );
+    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move |$(clone!(@param $p),)+| $body
+        }
+    );
+}
+
 pub enum Signal {}
 
 pub struct Ui {
@@ -35,7 +53,10 @@ impl Ui {
         ui.application.run(&[]);
     }
 
-    pub fn main_window(application: &gtk::Application, to_app: &mpsc::Sender<app::Signal>) -> gtk::ApplicationWindow {
+    pub fn main_window(
+        application: &gtk::Application,
+        to_app: &mpsc::Sender<app::Signal>,
+    ) -> gtk::ApplicationWindow {
         let window = gtk::ApplicationWindow::new(application);
         let container = gtk::HeaderBar::new();
 
@@ -50,30 +71,28 @@ impl Ui {
         window.set_size_request(500, 300);
 
         window.add_events(gdk::EventMask::KEY_PRESS_MASK);
-        let key_press_to_app = to_app.clone();
-        window.connect_key_press_event(move |_window, event| {
+        window.connect_key_press_event(clone!(to_app => move |_window, event| {
             if event.get_keyval() == gdk::enums::key::F5 {
-                key_press_to_app.send(app::Signal::RetrieveTimeEntries)
+                to_app.send(app::Signal::RetrieveTimeEntries)
                     .expect("Sending message to application thread");
                 Inhibit(true)
             } else if event.get_keyval() == gdk::enums::key::n {
-                key_press_to_app.send(app::Signal::OpenPopup)
+                to_app.send(app::Signal::OpenPopup)
                     .expect("Sending message to application thread");
                 Inhibit(true)
             } else {
                 Inhibit(false)
             }
-        });
+        }));
 
         let button =
             gtk::Button::new_from_icon_name(Some("list-add-symbolic"), gtk::IconSize::Button);
         button.set_sensitive(false);
         container.pack_start(&button);
-        let button_to_app = to_app.clone();
-        button.connect_clicked(move |_button| {
-            button_to_app.send(app::Signal::OpenPopup)
+        button.connect_clicked(clone!(to_app => move |_button| {
+            to_app.send(app::Signal::OpenPopup)
                 .expect("Sending message to application thread");
-        });
+        }));
 
         let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 2);
         hbox.set_spacing(0);
@@ -81,21 +100,19 @@ impl Ui {
         let prev_button =
             gtk::Button::new_from_icon_name(Some("go-previous-symbolic"), gtk::IconSize::Button);
         hbox.pack_start(&prev_button, false, false, 0);
-        let prev_button_to_app = to_app.clone();
-        prev_button.connect_clicked(move |_button| {
-            prev_button_to_app.send(app::Signal::PrevDate)
+        prev_button.connect_clicked(clone!(to_app => move |_button| {
+            to_app.send(app::Signal::PrevDate)
                 .expect("Sending message to application thread");
-        });
+        }));
 
         let next_button =
             gtk::Button::new_from_icon_name(Some("go-next-symbolic"), gtk::IconSize::Button);
         hbox.pack_start(&next_button, false, false, 0);
         container.pack_start(&hbox);
-        let next_button_to_app = to_app.clone();
-        next_button.connect_clicked(move |_button| {
-            next_button_to_app.send(app::Signal::NextDate)
+        next_button.connect_clicked(clone!(to_app => move |_button| {
+            to_app.send(app::Signal::NextDate)
                 .expect("Sending message to application thread");
-        });
+        }));
 
         window.show_all();
 
